bcd01f366fee63785de4241851d279a34b1f91e0
Jonathan Shekoni <jonathanshekoni1@gmail.com>
2026-06-06 17:43:39 -0400

    Add MenuBar component

---
+++ Frontend/src/Components/MenuBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Logo from '../Logo.png' 
+
+const MenuBar = () => {
+  return (
+    <nav className='w-full h-16  shadow-lg flex items-center justify-between px-6'>
+        <div className = 'flex items-center gap-2'> 
+            <div className='text-lg  text-blue-400  font-bebas '>SportsGA</div>
+            <img src={Logo} alt="Logo" className="rounded-lg shadow-md w-8 h-8" />
+        </div>
+
+
+        <div className='text-lg center font-bold'>Player Comparison</div>
+
+        <div className='w-24'>Search</div>
+    </nav>
+  )
+}
+
+export default MenuBar
