da6216c8308701f26278f753ed196e990f10f005
Jonathan Shekoni <jonathanshekoni1@gmail.com>
2026-06-08 22:58:21 -0400

    Add new player page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PlayerComparison from "./Components/PlayerComparison"
 import PlayerCard from "./Components/PlayerCard"
 import MenuBar from "./Components/MenuBar"
+import PlayerPage from "./Components/PlayerPage"
 
 
 function App() {
   return (
-    <div className="w-full">
-      <MenuBar />
-      <PlayerComparison />
-    </div>
+    <BrowserRouter>
+      <div className="w-full">
+        <MenuBar />
+        
+        <Routes>
+          <Route path="/" element={<PlayerComparison />} />
+          <Route path="/player" element={<PlayerPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ Frontend/src/Components/PlayerPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const PlayerPage = () => {
+  return (
+    <div>
+    <p className="text-lg font-bold my-4">PlayerPage</p>
+    </div>
+  )
+}
+
+
+export default PlayerPage
