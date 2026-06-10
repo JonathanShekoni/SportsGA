b09fa4f56276aae3a6736167855a3a9195854df5
Jonathan Shekoni <jonathanshekoni1@gmail.com>
2026-06-10 00:34:53 -0400

    Enhance MenuBar with player search suggestions and fetch player list; update PlayerPage to display player image

--- Frontend/src/Components/MenuBar.jsx
+++ Frontend/src/Components/MenuBar.jsx
@@ -1,14 +1,46 @@
 import React from 'react'
 import Logo from '../Logo.png' 
-import { useLocation,useNavigate,Link } from 'react-router-dom';
+import { useState,useEffect } from 'react'
+import { useLocation,useNavigate} from 'react-router-dom';
 
 const MenuBar = () => {
 
   const location = useLocation();
   const isTargetRoute = location.pathname === '/comparison';
+
+  const [playerList, setPlayerList] = useState([])
   
   const [searchTerm, setSearchTerm] = React.useState('');
   const navigate = useNavigate()
+
+  const [suggestions, setSuggestions] = useState([])
+
+
+  useEffect(() => {
+          const fetchplayers = async () => {
+              const res = await fetch(`https://sportsga.onrender.com/players`)
+              const data = await res.json()
+              setPlayerList(data)
+          }
+  
+          fetchplayers()
+      }, [])
+
+  const handleSuggestions = (typed) => {
+        const updatedList = []
+        
+        if (typed === ''){
+            setSuggestions([])
+            return
+        }
+        for (let i = 0; i < playerList.length; i++) {
+            if (playerList[i].toLowerCase().includes(typed.toLowerCase())) {
+                updatedList.push(playerList[i])
+            }
+
+        }
+        setSuggestions(updatedList)
+    }
 
   const SearchPlayer = (e) => {
     e.preventDefault()
@@ -26,15 +58,36 @@ const MenuBar = () => {
           <div className='text-lg center font-bold'>Player Comparison</div>
         )}
 
-        <div>
+        <div className='relative'>
           <form onSubmit = {SearchPlayer}>
             <input
               type = 'text'
               className = ' w-40 text-center border border-blue-400 rounded-2xl px-4 py-2 bg-transparent text-white placeholder-gray-400 focus:outline-none focus:border-blue-500'
               placeholder='Player Search'
               value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
+              onChange={(e) => { 
+                setSearchTerm(e.target.value)
+                handleSuggestions(e.target.value)
+              }}
+
+              
             />
+
+            {suggestions.length > 0 && (
+                        <div className='absolute w-full max-h-28 overflow-y-auto z-10'>
+                            {suggestions.map((player) => (
+                            <p 
+                                onClick={() =>{
+                                    setSearchTerm(player)
+                                    setSuggestions([])
+                                }} 
+                                key ={player}
+                                className='block w-full text-left hover:bg-blue-600 px-4 py-2 cursor-pointer'
+                                >{player}
+                            </p>
+                        ))}
+                        </div>
+                    )}
           </form>
         </div>
     </nav>
--- Frontend/src/Components/PlayerPage.jsx
+++ Frontend/src/Components/PlayerPage.jsx
@@ -21,6 +21,7 @@ const PlayerPage = () => {
   return (
     <div>
       <p className="text-lg font-bold my-4">{playerName}</p>
+      <img src={`https://cdn.nba.com/headshots/nba/latest/1040x760/${playerStats.id}.png`} className="w-32 h-32 object-cover mx-5  border-2 border-blue-400" />
       <p>{playerStats.id}</p>
     </div>
   )
